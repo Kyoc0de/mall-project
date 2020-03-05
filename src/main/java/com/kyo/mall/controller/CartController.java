83761df671c22ceb08e41ac6f76d21815497e52d
kyoo0 <kyoscode@gmail.com>
2020-03-05 18:14:51 -0500

    购物车模块controller开发完成 api接口基本完成

--- src/main/java/com/kyo/mall/controller/CartController.java
+++ src/main/java/com/kyo/mall/controller/CartController.java
@@ -1,19 +1,67 @@
 package com.kyo.mall.controller;
 
+import com.kyo.mall.consts.MallConst;
 import com.kyo.mall.form.CartAddForm;
+import com.kyo.mall.form.CartUpdateForm;
+import com.kyo.mall.pojo.User;
+import com.kyo.mall.service.ICartService;
 import com.kyo.mall.vo.CartVo;
 import com.kyo.mall.vo.ResponseVo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @RestController
 public class CartController {
 
+    @Autowired
+    private ICartService cartService;
+
+    @GetMapping("/carts")
+    public ResponseVo<CartVo> list(HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.list(user.getId());
+    }
+
     @PostMapping("/carts")
-    public ResponseVo<CartVo> add(@Valid @RequestBody CartAddForm cartAddForm) {
-        return null;
+    public ResponseVo<CartVo> add(@Valid @RequestBody CartAddForm cartAddForm,
+                                  HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.add(user.getId(), cartAddForm);
+    }
+
+    @PutMapping("/carts/{productId}")
+    public ResponseVo<CartVo> update(@PathVariable Integer productId,
+                                     @Valid @RequestBody CartUpdateForm form,
+                                     HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.update(user.getId(), productId, form);
+    }
+
+    @DeleteMapping("/carts/{productId}")
+    public ResponseVo<CartVo> delete(@PathVariable Integer productId,
+                                     HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.delete(user.getId(), productId);
+    }
+
+    @PutMapping("/carts/selectAll")
+    public ResponseVo<CartVo> selectAll(HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.selectAll(user.getId());
+    }
+
+    @PutMapping("/carts/unSelectAll")
+    public ResponseVo<CartVo> unSelectAll(HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.unSelectAll(user.getId());
+    }
+
+    @GetMapping("/carts/products/sum")
+    public ResponseVo<Integer> sum(HttpSession session) {
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.sum(user.getId());
     }
 }
